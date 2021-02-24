b2bf3f95621814734e8626e017449dbc1df8f3f1
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-02-24 16:19:06 +0700

    Set default version

--- cr.sh
+++ cr.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DEFAULT_CHART_RELEASER_VERSION=v1.0.0
+DEFAULT_CHART_RELEASER_VERSION=v1.2.1-kartoza
 
 : "${CR_TOKEN:?Environment variable CR_TOKEN must be set}"
 
